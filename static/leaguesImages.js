74b54885ed102a5c761b50513a8feecc779eda43
Aleksander Shumeiko <music.dll@mail.ru>
2021-06-30 17:35:26 +0300

    deploy

---
+++ static/leaguesImages.js
@@ -0,0 +1,14 @@
+export let leaguesImages = {
+  2001: 'https://pbs.twimg.com/media/EkolIV-XUAU-rty.jpg',
+  2017: 'https://i.ytimg.com/vi/rBHznc3fbbM/maxresdefault.jpg',
+  2021: 'https://www.aravot.am/wp-content/uploads/2020/04/Karantin-1-e1587908045366.jpg',
+  2003: 'https://www.fifplay.com/img/public/fifa-17-team-of-the-season-eredivisie.jpg',
+  2002: 'https://1.bp.blogspot.com/-H9SOuYdvKI4/XVNDxpvQ6_I/AAAAAAAAAuo/WodE8F0xzUY_jN-0T3iTujYlP_TyqfBeQCPcBGAYYCw/s640/thumb2-bundesliga-4k-logo-silk-texture-german-football-league.jpg',
+  2015: 'https://pbs.twimg.com/media/B4CLSXmCcAEQp34.jpg',
+  2019: 'https://telegra.ph/file/aac84d6102f866c6196b6.jpg',
+  2014: 'https://pbs.twimg.com/media/Eev6iUFVAAM8QaL.jpg',
+  2016: 'https://terrikon.com/i/l/1200/eng/cefl.jpg',
+  2013: 'https://i.ytimg.com/vi/0Vz5HyLadb8/maxresdefault.jpg',
+  2018: 'https://10wallpaper.com/wallpaper/1920x1200/1910/2020_UEFA_European_Football_Cup_2019_Featured_Desktop_1920x1200.jpg',
+  2000: 'https://media.metrolatam.com/2019/09/03/capturadepantall-ecf4a20cd4262b5c3db97744e2aa384d-1200x800.jpg'
+}
